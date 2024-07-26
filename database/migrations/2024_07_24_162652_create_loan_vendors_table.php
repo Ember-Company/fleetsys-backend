d5c58aab44417b9cb256d3ed5e113db8317274a2
Mamonha <mamonha302@gmail.com>
2024-07-26 13:33:08 -0300

    Adjusting loan_vendors migration

--- database/migrations/2024_07_24_162652_create_loan_vendors_table.php
+++ database/migrations/2024_07_24_162652_create_loan_vendors_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('loan_vendors', function (Blueprint $table) {
-            $table->uuid();
+            $table->uuid('id')->primary();
             $table->string('name');
             $table->timestamps();
         });
