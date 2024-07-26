3d09969b87ec5aff11ce5d6e325cc396511a91ce
Mamonha <mamonha302@gmail.com>
2024-07-26 13:32:43 -0300

    Adjusting vehicle_status migration

--- database/migrations/2024_07_24_161859_create_vehicle_status_table.php
+++ database/migrations/2024_07_24_161859_create_vehicle_status_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('vehicle_status', function (Blueprint $table) {
-            $table->uuid();
+            $table->uuid('id')->primary();
             $table->unsignedInteger('status');
             $table->string('color');
             $table->timestamps();
