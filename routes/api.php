6e442a7f3ce5756894a97687a376f477ae9c67dc
Mamonha <mamonha302@gmail.com>
2024-07-28 19:43:21 -0300

    Creating company/fuel endpoints and adding auth bearer token in routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\FuelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -11,3 +13,8 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function (){
+    Route::resource('company', CompanyController::class)->except('edit','create');
+    Route::resource('fuel', FuelController::class)->except('edit','create');
+});
