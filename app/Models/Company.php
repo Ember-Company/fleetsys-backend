038f8fc5ffb4ee501e977750f13d6b9638f02a91
Mamonha <mamonha302@gmail.com>
2024-07-28 19:26:59 -0300

    Creating company model adding HasUuids concern

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
+
+    protected $table = 'company';
+
+    protected $fillable = ['name', 'created_at'];
+
+    protected $casts = ['id' => 'string'];
+
 }
