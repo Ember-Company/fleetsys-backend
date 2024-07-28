5b80e7d7a2300af856584dc119e22ed0353bd750
Mamonha <mamonha302@gmail.com>
2024-07-28 19:24:31 -0300

    Creating company controller

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCompanyRequest;
+use App\Http\Requests\UpdateCompanyRequest;
+use App\Models\Company;
+use Exception;
+
+class CompanyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        try{
+            $companies = Company::all();
+        
+            return response()->json(['data' => $companies], 200); 
+        }catch(Exception $e){
+            return response()->json(['error' => $e->getMessage()], 500); 
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCompanyRequest $request)
+     {
+        try{
+            $data = $request->validated(); 
+            Company::create($data);
+
+            return response()->json(['success' => 'Company successfully created'], 200);
+        }catch(Exception $e){
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Company $company)
+    {
+        try{
+            return response()->json(['data' => $company ], 200);
+        }catch(Exception $e){
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCompanyRequest $request, Company $company)
+    {
+        try{
+            $data = $request->validated();
+            $company->update($data);
+            
+            return response()->json(['success' => 'Company data successfully updated' ], 200);
+        }catch(Exception $e){
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+        
+    
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Company $company)
+    {
+        try{
+            $company->delete();
+            return response()->json(['success' => "Company " . $company->name." successfully removed" ], 200);
+        }catch(Exception $e){
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+}
