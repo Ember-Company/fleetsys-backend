76b0208277821953a9e22edceca5d8f6705c05ab
Mamonha <mamonha302@gmail.com>
2024-07-29 13:31:12 -0300

    Wip: CurrentLocation controller

---
+++ app/Http/Controllers/CurrentLocationController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCurrentLocationRequest;
+use App\Http\Requests\UpdateCurrentLocationRequest;
+use App\Models\CurrentLocation;
+
+class CurrentLocationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCurrentLocationRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CurrentLocation $currentLocation)
+    {
+        //
+    }
+}
