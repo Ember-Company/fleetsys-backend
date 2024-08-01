f1b1dac8e5fd01311607a036492ded01dc05e34f
Mamonha <mamonha302@gmail.com>
2024-08-01 20:30:52 -0300

    Adding soft delete in current_location table

--- database/migrations/2024_07_24_161632_create_current_location_table.php
+++ database/migrations/2024_07_24_161632_create_current_location_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->uuid('id')->primary();
             $table->decimal('latitude', 10, 7);
             $table->decimal('longitude', 10, 7);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
