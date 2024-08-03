1a33f77863e85fa1bdd2377093e3638c4f551642
dannyk-dev <kruger.dkk@gmail.com>
2024-08-03 03:40:23 -0300

    refactor: added custom auth requests

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,21 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AuthLoginRequest;
+use App\Http\Requests\AuthRegisterRequest;
 use App\Models\User;
-use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
-use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class AuthController extends Controller
 {
     use AuthorizesRequests;
 
-    public function register(Request $request)
+    public function register(AuthRegisterRequest $request)
     {
         $company_query = \App\Models\Company::query();
 
@@ -29,11 +28,7 @@ class AuthController extends Controller
         $this->authorize('is-member', $company); // if the current user and the new user are from the same company allow user creation
 
         $user = User::create([
-            ...$request->validate([
-                'name' => 'required|string|max:255',
-                'email' => 'required|string|email|max:255|unique:users,email',
-                'password' => 'required|string|min:8',
-            ]),
+            ...$request->validated(),
             'company_id' => $request->user()->company->id
         ]);
 
@@ -45,14 +40,12 @@ class AuthController extends Controller
         // return response()->json(['access_token' => $token,'token_type' => 'Bearer', 'user' => $user], 200);
     }
 
-    public function login(Request $request)
+    public function login(AuthLoginRequest $request)
     {
-        $request->validate([
-            'email' => 'required|email',
-            'password' => 'required'
-        ]);
+        $request->validated();
 
-        if (!Auth::attempt($request->only('email', 'password'))) {
+        if (!Auth::attempt($request->only('email', 'password')))
+        {
             throw ValidationException::withMessages([
                 'message' => 'invalid credentials'
             ]);
