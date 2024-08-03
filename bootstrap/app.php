b6a48e0d5e63766ae3fd6f159fcc0a7decad8331
dannyk-dev <kruger.dkk@gmail.com>
2024-08-03 03:21:57 -0300

    feat: handle resource/model not found exceptions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +17,12 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->wantsJson())
+            {
+                return response()->json([
+                    'error' => 'Resource not found'
+                ], $e->getStatusCode());
+            }
+        });
     })->create();
