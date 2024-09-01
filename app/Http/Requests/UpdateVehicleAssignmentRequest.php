86c0c538c29fc0c4e511c033a8dd76bf08969b9a
Mamonha <mamonha302@gmail.com>
2024-09-01 00:00:39 -0300

    Defining assignment requests

---
+++ app/Http/Requests/UpdateVehicleAssignmentRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateVehicleAssignmentRequest extends FormRequest
+{
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'started_at' => ['required', 'date', 'before_or_equal:ended_at'],
+            'ended_at' => ['nullable', 'date', 'after_or_equal:started_at'],
+            'starting_meter_entry' => ['nullable', 'string', 'max:255'],               
+            'ending_meter_entry' => ['nullable', 'string', 'max:255'],
+            'comments' => ['nullable', 'string'],
+            'user_id' => ['required','exists:users,id'],                             
+            'vehicle_id' => ['required','exists:vehicles,id'],                     
+        ];
+    }
+}
