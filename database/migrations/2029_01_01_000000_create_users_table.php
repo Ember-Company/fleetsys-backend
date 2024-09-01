cb5188824ba4a9039033873448104d679a1c9ced
Mamonha <mamonha302@gmail.com>
2024-09-01 00:03:18 -0300

    Adding profile_image field in user migration

--- database/migrations/2029_01_01_000000_create_users_table.php
+++ database/migrations/2029_01_01_000000_create_users_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
             $table->string('phone_number')->nullable();
             // $table->unsignedInteger('role');
             $table->string('role')->default(UserRole::USER);
-
+            $table->string('profile_image')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
