02ab19b3a91b9a52f2f86a634341b24c2b23e369
dannyk-dev <kruger.dkk@gmail.com>
2024-09-06 18:25:57 -0300

    temporarily barred interval updates

--- app/Listeners/UpdateCompanyActivity.php
+++ app/Listeners/UpdateCompanyActivity.php
@@ -24,14 +24,15 @@ class UpdateCompanyActivity
         $user = $event->user;
         $company = $user->company;
 
-        $lastActiveAt = $company->last_active_at ? $company->last_active_at : now()->subDays(1);
+        // $lastActiveAt = $company->last_active_at ? $company->last_active_at : now()->subDays(1);
 
-        $lastActiveAt = $lastActiveAt instanceof \Carbon\Carbon ? $lastActiveAt : \Carbon\Carbon::parse($lastActiveAt);
+        // $lastActiveAt = $lastActiveAt instanceof \Carbon\Carbon ? $lastActiveAt : \Carbon\Carbon::parse($lastActiveAt);
 
-        if ($lastActiveAt->lt(now()->subMinutes(5))) {
-            $company->update([
-                'last_active_at' => now()
-            ]);
-        }
+        // if ($lastActiveAt->lt(now()->subMinutes(5))) {
+
+        // }
+        $company->update([
+            'last_active_at' => now()
+        ]);
     }
 }
