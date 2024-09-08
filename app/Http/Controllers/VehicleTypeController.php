fc0aee686a21408870956e4a260843a3820c7294
Mamonha <mamonha302@gmail.com>
2024-09-08 11:53:20 -0300

    Adapting structure to allow an attribute to be associated with a vehicle type store/update methods

--- app/Http/Controllers/VehicleTypeController.php
+++ app/Http/Controllers/VehicleTypeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreVehicleTypeRequest;
+use App\Http\Requests\UpdateVehicleTypeRequest; 
 use App\Http\Resources\StandardResource;
+use App\Models\Attribute;
 use App\Models\VehicleType;
 use App\Traits\ValidatesUniques;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
@@ -33,22 +36,27 @@ class VehicleTypeController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreVehicleTypeRequest $request)
     {
         $company = $request->user()->company;
-
-        $vehicleType = $company->vehicleTypes()->create([
-            ...$request->validate([
-                'name' => [
-                    'required',
-                    $this->uniqueWithCompany('vehicle_types', 'name')
-                ],
-            ]),
+    
+        $data = $request->validated();
+    
+        $vehicleType = $company->vehicleTypes()->update([
+            'name' => $data['name'],
         ]);
+    
+        $attributes = $data['attributes'] ?? [];
+    
+        foreach ($attributes as $attributeName) {
+            $attribute = Attribute::firstOrCreate(['name' => $attributeName]);
+    
+            $vehicleType->attributes()->syncWithoutDetaching([$attribute->id]);
+        }
 
-        return new StandardResource($vehicleType);
+        return new StandardResource($vehicleType->load('attributes'));
     }
-
+    
     /**
      * Display the specified resource.
      */
@@ -62,18 +70,25 @@ class VehicleTypeController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, VehicleType $vehicleType)
-    {
-        $vehicleType->update([
-            ...$request->validate([
-                'name' => [
-                    'sometimes',
-                    $this->uniqueWithCompany('vehicle_types', 'name')
-                ]
-            ])
-        ]);
 
-        return new StandardResource($vehicleType);
+    public function update(UpdateVehicleTypeRequest $request, VehicleType $vehicleType)
+    {
+        $data = $request->validated();
+
+        if (isset($data['name'])) {
+            $vehicleType->update(['name' => $data['name']]);
+        }
+
+        $attributes = collect($data['attributes'] ?? []);
+
+        $attributeIds = $attributes->map(function ($attributeName) {
+            $attribute = Attribute::firstOrCreate(['name' => $attributeName]);
+            return $attribute->id;
+        })->toArray();
+
+        $vehicleType->attributes()->sync($attributeIds);
+
+        return new StandardResource($vehicleType->load('attributes'));
     }
 
     /**
