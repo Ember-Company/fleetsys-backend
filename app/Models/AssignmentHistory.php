be6c563cc983054d67f13bbd98aea62a93db700c
Mamonha <mamonha302@gmail.com>
2024-10-20 22:57:24 -0300

    Defining user and vehicle AssignmentHistory relations

--- app/Models/AssignmentHistory.php
+++ app/Models/AssignmentHistory.php
@@ -4,11 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class AssignmentHistory extends Model
 {
     use HasFactory;
     protected $table = 'assignments_history';
     protected $fillable = ['user_id', 'vehicle_id', 'action'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function vehicle(): BelongsTo
+    {
+        return $this->belongsTo(Vehicle::class);
+    }
 
 }
