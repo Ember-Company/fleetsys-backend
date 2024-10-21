ede6177cccba44e4fe11b38e18af6c8defbe446f
Mamonha <mamonha302@gmail.com>
2024-10-20 22:56:58 -0300

    Defining assignmenthistories relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -93,4 +93,10 @@ class User extends Authenticatable
     {
         return $this->hasMany(VehicleAssignment::class);
     }
+
+    public function assignmentHistories()
+    {
+        return $this->hasMany(AssignmentHistory::class);
+    }
+
 }
